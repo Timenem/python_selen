9117f0d7f11efdb38ce27d2d610f63e48a4a623e
Timenem <56394536+Timenem@users.noreply.github.com>
2022-04-22 19:45:47 +0000

    Create quest81.py

---
+++ quests/quest81.py
@@ -0,0 +1,62 @@
+MORSE = {
+    ".-": "a",
+    "-...": "b",
+    "-.-.": "c",
+    "-..": "d",
+    ".": "e",
+    "..-.": "f",
+    "--.": "g",
+    "....": "h",
+    "..": "i",
+    ".---": "j",
+    "-.-": "k",
+    ".-..": "l",
+    "--": "m",
+    "-.": "n",
+    "---": "o",
+    ".--.": "p",
+    "--.-": "q",
+    ".-.": "r",
+    "...": "s",
+    "-": "t",
+    "..-": "u",
+    "...-": "v",
+    ".--": "w",
+    "-..-": "x",
+    "-.--": "y",
+    "--..": "z",
+
+    "-----": "0",
+    ".----": "1",
+    "..---": "2",
+    "...--": "3",
+    "....-": "4",
+    ".....": "5",
+    "-....": "6",
+    "--...": "7",
+    "---..": "8",
+    "----.": "9",
+}
+
+
+
+def morse_decoder(code):
+    answer = ''
+    for i in code.split(' '):
+        if i in MORSE:
+            answer += MORSE[i]
+        elif i == '' and answer[-1] != ' ':
+            answer += ' '
+    return answer.capitalize()
+
+
+if __name__ == "__main__":
+    print(morse_decoder("... --- ..."))
+    print(morse_decoder("... --- -- .   - . -..- -"))
+
+    # These "asserts" using only for self-checking and not necessary for auto-testing
+    assert morse_decoder("... --- -- .   - . -..- -") == "Some text"
+    assert morse_decoder("..--- ----- .---- ---..") == "2018"
+    assert (morse_decoder(".. -   .-- .- ...   .-   --. --- --- -..   -.. .- -.--")
+        == "It was a good day")
+    print("Coding complete? Click 'Check' to earn cool rewards!")
