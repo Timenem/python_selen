46bf29c8c7b0bcff8dfb47931aa14821aba5b733
Timenem <56394536+Timenem@users.noreply.github.com>
2022-05-21 08:40:47 +0000

    Create test_sorting.py

---
+++ sortAlghortirms/test_sorting.py
@@ -0,0 +1,21 @@
+from SortingAlg import BubbleSort
+import unittest
+
+rnd = [5, -2, 4, -1, 0]
+answer = [-2, -1, 0, 4, 5]
+reversAnswer = sorted(answer, reverse=True)
+
+
+class TestSort(unittest.TestCase):
+    def setUp(self):
+        self.sortAlg = BubbleSort()
+
+    def testForvardSort(self):
+        self.assertEqual(self.sortAlg.forvardSort(rnd), answer)
+
+    def testReversedSort(self):
+        self.assertEqual(self.sortAlg.reveresdSort(rnd), reversAnswer)
+
+
+if __name__ == '__main__':
+    unittest.main()
