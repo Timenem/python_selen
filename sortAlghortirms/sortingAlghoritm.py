a812814f51f1188e8fc74e0f2324e8cefc858fe0
Timenem <56394536+Timenem@users.noreply.github.com>
2022-05-21 09:21:52 +0000

    added insertion sort

--- sortAlghortirms/sortingAlghoritm.py
+++ sortAlghortirms/sortingAlghoritm.py
@@ -18,3 +18,22 @@ class BubbleSort:
                     randomIntegerList[j], randomIntegerList[j + 1] = randomIntegerList[j + 1], randomIntegerList[j]
         print(randomIntegerList)
         return randomIntegerList
+
+    
+    
+ class InsertionSort:
+    def insertSort(self, randomIntegerList:List[int]):
+        n = len(randomIntegerList)
+        for i in range(1 ,n):
+            currentVal = randomIntegerList[i]
+            j = i -1
+            while j >=0:
+                if currentVal < randomIntegerList[j]:
+                    randomIntegerList[j+1] = randomIntegerList[j]
+                    randomIntegerList[j] = currentVal
+                    j = j-1
+                else:
+                    break
+        return randomIntegerList
+
+   
