0537563e6e0a0c63e563eddff42bb0bdc8797c1a
Timenem <56394536+Timenem@users.noreply.github.com>
2022-06-29 17:03:40 +0000

    Create quest100.py

---
+++ quests/quest100.py
@@ -0,0 +1,33 @@
+"""
+In this mission you need to create a password verification function.
+Those are the verification conditions:
+    the length should be bigger than 6;
+    should contain at least one digit, but it cannot consist of just digits;
+    if the password is longer than 9 - previous rule (about one digit), is not required.
+Input: A string.
+Output: A bool. 
+"""
+
+
+
+def is_acceptable_password(p):
+   if len(p) in range(7,10):
+      return (any(i.isdigit() for i in p) and not p.isdigit())
+   elif len(p) > 9:
+      return True
+   else:
+      return False
+
+
+
+if __name__ == "__main__":
+    # These "asserts" are used for self-checking and not for an auto-testing
+    assert is_acceptable_password("short") == False
+    assert is_acceptable_password("short54") == True
+    assert is_acceptable_password("muchlonger") == True
+    assert is_acceptable_password("ashort") == False
+    assert is_acceptable_password("muchlonger5") == True
+    assert is_acceptable_password("sh5") == False
+    assert is_acceptable_password("1234567") == False
+    assert is_acceptable_password("12345678910") == True
+    print("Coding complete? Click 'Check' to earn cool rewards!")
