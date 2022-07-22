f00c9e0e87903ceee708521f3550b916cb82599d
Timenem <56394536+Timenem@users.noreply.github.com>
2022-07-22 10:39:36 +0000

    sort odd number in list

--- quests/quest116.py
+++ quests/quest116.py
@@ -6,7 +6,6 @@ Examples
 [9, 8, 7, 6, 5, 4, 3, 2, 1, 0]  =>  [1, 8, 3, 6, 5, 4, 7, 2, 9, 0]
 """
 
-
 def sort_array(source_array):
     odd_numbers = sorted([i for i in source_array if i%2!=0])
     c = 0
