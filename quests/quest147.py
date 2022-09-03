786b39461199100f3e568e5896083c1ad680ccba
Timenem <56394536+Timenem@users.noreply.github.com>
2022-09-03 16:03:01 +0000

    Create quest147.py

---
+++ quests/quest147.py
@@ -0,0 +1,23 @@
+"""
+ In this mission you need to create a password verification function.
+
+Those are the verification conditions:
+
+    the length should be bigger than 6;
+    should contain at least one digit, but it cannot consist of just digits;
+    having numbers or containing just numbers does not apply to the password longer than 9.
+    a string should not contain the word "password" in any case.
+
+Input: A string.
+
+Output: A bool. 
+
+"""
+
+
+def is_acceptable_password(s:str)->bool:
+    one = len(s) > 6
+    two = any(map(str.isdigit,s)) and not s.isdigit()
+    three = len(s) > 9
+    four = 'password' not in s.lower()
+    return  one and (two or three) and four
