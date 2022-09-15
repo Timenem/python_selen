49b75738985535280146a10569185b40bd56b50b
Timenem <56394536+Timenem@users.noreply.github.com>
2022-09-15 18:20:24 +0000

    Update quest160.py

--- quests/quest160.py
+++ quests/quest160.py
@@ -1,3 +1,14 @@
+"""
+You are given a list/array which contains only integers (positive and negative). Your job is to sum only the numbers that are the same and consecutive. The result should be one list.
+
+Extra credit if you solve it in one line. You can assume there is never an empty list/array and there will always be an integer.
+
+Same meaning: 1 == 1
+
+1 != -1
+"""
+
+
 
 from itertools import  groupby
 def sum_consecutives(s:list)->list:
