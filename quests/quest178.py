ffb551e9d8efdc077767d6bfceb905a1e041d525
Timenem <56394536+Timenem@users.noreply.github.com>
2022-11-05 17:39:43 +0000

    Create quest178.py

---
+++ quests/quest178.py
@@ -0,0 +1,25 @@
+"""
+
+
+Oh thank goodness you're here! The last intern has completely ruined everything!
+
+All of our customer's phone numbers have been scrambled, and we need those phone numbers to annoy them with endless sales calls!
+The Format
+
+Phone numbers are stored as strings and comprise 11 digits, eg '02078834982' and must always start with a 0.
+
+However, something strange has happened and now all of the phone numbers contain lots of random characters, whitespace and some are not phone numbers at all!
+
+For example, '02078834982' has somehow become 'efRFS:)0207ERGQREG88349F82!' and there are lots more lines that we need to check.
+The Task
+
+Given a string, you must decide whether or not it contains a valid phone number. If it does, return the corrected phone number as a string ie. '02078834982' with no whitespace or special characters, else return "Not a phone number".
+
+"""
+
+def is_it_a_num(s:str)->str:
+    digits= [i for i in s if i.isdigit()]
+    if len(digits) == 11 and digits[0] =='0':
+        return ("".join(digits))
+    else:
+        return ("Not a phone number")
