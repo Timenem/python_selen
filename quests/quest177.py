991e0b564d7e516d89de6c24868d3f1d842132f5
Timenem <56394536+Timenem@users.noreply.github.com>
2022-11-05 16:13:39 +0000

    Create quest177.py

---
+++ quests/quest177.py
@@ -0,0 +1,12 @@
+'''
+Write a function that takes a string and outputs a strings of 1's and 0's where vowels become 1's and non-vowels become 0's.
+All non-vowels including non alpha characters (spaces,commas etc.) should be included.
+Examples:
+vowelOne "abceios" -- "1001110"
+vowelOne "aeiou, abc" -- "1111100100"
+'''
+
+
+def vowel_one(s):
+    vowels = 'aeiou'
+    return ''.join('1' if c in vowels else '0' for c in s.lower())
