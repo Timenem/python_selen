dbf1d2fa59a155b401ee363a6edc2a18a1f7db02
Timenem <56394536+Timenem@users.noreply.github.com>
2022-12-24 22:07:14 +0300

    Create quest228.py

---
+++ quests/quest228.py
@@ -0,0 +1,13 @@
+"""
+Write a function that when given a number >= 0, returns an Array of ascending length subarrays.
+pyramid(0) => [ ]
+pyramid(1) => [ [1] ]
+pyramid(2) => [ [1], [1, 1] ]
+pyramid(3) => [ [1], [1, 1], [1, 1, 1] ]
+"""
+
+def pyramid(i:int):
+    arr = []
+    for x in range(1,i+1):
+        arr.append([int(i) for i in (str(1) * x)])
+    return arr
