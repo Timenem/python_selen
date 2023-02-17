fd55bb61c6ed49a95512bd587fea312467348840
Timenem <56394536+Timenem@users.noreply.github.com>
2023-02-17 17:19:55 +0000

    Create quest305.py

---
+++ quests/quest305.py
@@ -0,0 +1,23 @@
+"""
+Given an array, return the reversed version of the array (a different kind of reverse though), you reverse portions of the array, you'll be given a length argument which represents the length of each portion you are to reverse.
+
+selReverse([1,2,3,4,5,6], 2)
+ //=> [2,1, 4,3, 6,5]
+if after reversing some portions of the array and the length of the remaining portion in the array is not up to the length argument, just reverse them.
+selReverse([2,4,6,8,10,12,14,16], 3)
+ //=> [6,4,2, 12,10,8, 16,14]
+selReverse(array, length)
+    array - array to reverse
+    length - length of each portion to reverse
+Note : if the length argument exceeds the array length, reverse all of them, if the length argument is zero do not reverse at all
+"""
+from itertools import chain
+def sel_reverse(arr:list[int], l:int)->list[int]:
+    ans = []
+    if l > len(arr):
+        return list(reversed(arr))
+    if l == 0 :
+        return arr
+    for i in range(0,len(arr),l):
+        ans.append( arr[i:i+l][::-1])
+    return [j for i in ans for j in i]
