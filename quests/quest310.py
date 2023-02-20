941905c795bf9f6cd8bad417aacc8bf4dde25040
Timenem <56394536+Timenem@users.noreply.github.com>
2023-02-20 13:35:03 +0000

    Create quest310.py

---
+++ quests/quest310.py
@@ -0,0 +1,14 @@
+"""
+Several people are standing in a row divided into two teams.
+The first person goes into team 1, the second goes into team 2, the third goes into team 1, and so on.
+Task
+
+Given an array of positive integers (the weights of the people), return a new array/tuple of two integers, where the first one is the total weight of team 1, and the second one is the total weight of team 2.
+"""
+
+def row_weights(array):
+    team_a = sum(array[::2])
+    team_b = sum(array[1::2])
+    return (team_a,team_b)
+  
+  
