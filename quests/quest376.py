238ed35aeed391e3e11de38eb0b13daab1b0a7e0
Timenem <56394536+Timenem@users.noreply.github.com>
2023-05-25 08:25:04 +0000

    Create quest376.py

---
+++ quests/quest376.py
@@ -0,0 +1,15 @@
+def what_century(year):
+    if year[2:] == "00":
+        century = year[:2]
+    else:
+        century = str(int(year[0:2]) + 1)
+    suffix = "th"
+    if century[0] == "1":
+        pass
+    elif century[1] == "1":
+        suffix = "st"
+    elif century[1] == "2":
+        suffix = "nd"
+    elif century[1] == "3":
+        suffix = "rd"
+    return century + suffix
